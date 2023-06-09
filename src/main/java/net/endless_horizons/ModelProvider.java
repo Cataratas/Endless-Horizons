7199ff20731da3cb7b07088ea5a94e82855d365b
Bernardo Facchi <cataratasdolly@outlook.com>
2023-06-09 13:56:41 -0300

    Trapdoors

--- src/main/java/net/endless_horizons/ModelProvider.java
+++ src/main/java/net/endless_horizons/ModelProvider.java
@@ -1,6 +1,7 @@
 package net.endless_horizons;
 
 import net.endless_horizons.blocks.models.DoorModel;
+import net.endless_horizons.blocks.models.TrapdoorModel;
 import net.fabricmc.fabric.api.client.model.ModelProviderContext;
 import net.fabricmc.fabric.api.client.model.ModelResourceProvider;
 import net.endless_horizons.blocks.models.BlockModel;
@@ -8,10 +9,6 @@ import net.minecraft.client.render.model.UnbakedModel;
 import net.minecraft.util.DyeColor;
 import net.minecraft.util.Identifier;
 
-import java.util.Arrays;
-import java.util.List;
-
-import static java.util.Arrays.asList;
 import static net.endless_horizons.EndlessHorizons.MOD_ID;
 
 public class ModelProvider implements ModelResourceProvider {
@@ -24,6 +21,8 @@ public class ModelProvider implements ModelResourceProvider {
                 return new BlockModel(color);
             } else if (identifier.equals(new Identifier(MOD_ID + ":block/endless_" + color + "_door_model"))) {
                 return new DoorModel(color);
+            } else if (identifier.equals(new Identifier(MOD_ID + ":block/endless_" + color + "_trapdoor_model"))) {
+                return new TrapdoorModel(color);
             }
         }
         return null;
