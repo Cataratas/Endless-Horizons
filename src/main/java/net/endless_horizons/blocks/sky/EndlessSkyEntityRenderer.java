7811213b1d6a919906e1949748af5a5a9fea89e5
Bernardo Facchi <cataratasdolly@outlook.com>
2023-07-08 03:30:17 -0300

    Fixed sky render

--- src/main/java/net/endless_horizons/blocks/sky/EndlessSkyEntityRenderer.java
+++ src/main/java/net/endless_horizons/blocks/sky/EndlessSkyEntityRenderer.java
@@ -1,24 +1,13 @@
 package net.endless_horizons.blocks.sky;
 
-import com.mojang.blaze3d.platform.GlConst;
-import com.mojang.blaze3d.platform.GlStateManager;
-import com.mojang.blaze3d.systems.RenderSystem;
-import net.endless_horizons.blocks.EndlessEntity;
 import net.endless_horizons.blocks.EndlessRenderer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.*;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.Vec3d;
-import org.joml.Matrix4f;
-
-import java.util.Objects;
 
 import static net.endless_horizons.client.EndlessHorizonsClient.skyRenderLayer;
-import static net.endless_horizons.client.EndlessHorizonsClient.skyShaderProgram;
 
 
 @Environment(EnvType.CLIENT)
@@ -27,34 +16,6 @@ public class EndlessSkyEntityRenderer extends EndlessRenderer<EndlessSkyEntity> 
     @Override
     public void render(EndlessSkyEntity entity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
         renderSides(entity, matrices.peek().getPositionMatrix(), vertexConsumers.getBuffer(skyRenderLayer));
-
-        //RenderSystem.setShader(() -> skyShaderProgram);
-        //RenderSystem.clear(GlConst.GL_DEPTH_BUFFER_BIT | GlConst.GL_COLOR_BUFFER_BIT, MinecraftClient.IS_SYSTEM_MAC);
-//
-        //MinecraftClient client = MinecraftClient.getInstance();
-        //Camera camera = client.gameRenderer.getCamera();
-        //assert client.world != null;
-        //Vec3d cameraPos = camera.getPos();
-        //boolean bl32 = client.world.getDimensionEffects().useThickFog(MathHelper.floor(cameraPos.getX()), MathHelper.floor(cameraPos.getY())) || client.inGameHud.getBossBarHud().shouldThickenFog();
-//
-        //Matrix4f matrix4f = matrices.peek().getPositionMatrix();
-        //BackgroundRenderer.render(camera, tickDelta, client.world, client.options.getClampedViewDistance(), client.gameRenderer.getSkyDarkness(tickDelta));
-        //BackgroundRenderer.setFogBlack();
-        //client.worldRenderer.renderSky(matrices, matrix4f, client.getTickDelta(), camera, false, () ->
-        //        BackgroundRenderer.applyFog(camera, BackgroundRenderer.FogType.FOG_SKY, client.gameRenderer.getViewDistance(), bl32, tickDelta));
-//
-        //MatrixStack modelViewStack = RenderSystem.getModelViewStack();
-        //modelViewStack.push();
-        //modelViewStack.multiplyPositionMatrix(matrix4f);
-        //RenderSystem.applyModelViewMatrix();
-//
-        //BufferBuilder buffer = Tessellator.getInstance().getBuffer();
-        //buffer.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION);
-        //renderSides(entity, matrix4f, buffer);
-        //BufferRenderer.drawWithGlobalProgram(buffer.end());
-//
-        //modelViewStack.pop();
-        //RenderSystem.applyModelViewMatrix();
     }
 
     @Override
--- src/main/java/net/endless_horizons/client/EndlessHorizonsClient.java
+++ src/main/java/net/endless_horizons/client/EndlessHorizonsClient.java
@@ -1,30 +1,19 @@
 package net.endless_horizons.client;
 
-import com.mojang.blaze3d.platform.TextureUtil;
 import com.mojang.blaze3d.systems.RenderSystem;
-import ladysnake.satin.api.event.PostWorldRenderCallbackV2;
 import ladysnake.satin.api.event.ResolutionChangeCallback;
-import ladysnake.satin.impl.FramebufferWrapper;
 import net.endless_horizons.ModelProvider;
 import net.endless_horizons.blocks.sky.EndlessSkyEntityRenderer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.model.ModelLoadingRegistry;
 import net.endless_horizons.blocks.end_gateway.EndlessEndEntityRenderer;
 import net.fabricmc.fabric.api.client.rendering.v1.CoreShaderRegistrationCallback;
-import net.fabricmc.fabric.api.renderer.v1.Renderer;
-import net.minecraft.client.MinecraftClient;
+import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 import net.minecraft.client.gl.ShaderProgram;
 import net.minecraft.client.gl.SimpleFramebuffer;
-import net.minecraft.client.gl.VertexBuffer;
 import net.minecraft.client.render.*;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
-import net.minecraft.client.render.RenderPhase.TextureBase;
-import net.minecraft.client.texture.Sprite;
-import net.minecraft.client.texture.TextureStitcher;
-import net.minecraft.client.util.Window;
 import net.minecraft.util.Identifier;
-
-import java.util.function.Supplier;
 
 import static net.endless_horizons.EndlessHorizons.*;
 
@@ -32,13 +21,12 @@ import static net.endless_horizons.EndlessHorizons.*;
 @net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
 public class EndlessHorizonsClient implements ClientModInitializer {
     public static ShaderProgram skyShaderProgram;
-    //public static VertexBuffer skyBuffer;
-    public static SimpleFramebuffer skyTexture;
+    public static SimpleFramebuffer skyFramebuffer;
 
     public static ShaderProgram getSkyShader() { return skyShaderProgram; }
 
-    private static void setSkyTexture() { RenderSystem.setShaderTexture(0, skyTexture != null ? skyTexture.getColorAttachment() : 0); }
-    //texture(RenderPhase.Textures.create().add(EndPortalBlockEntityRenderer.SKY_TEXTURE, false, false)
+    private static void setSkyTexture() { RenderSystem.setShaderTexture(0, skyFramebuffer != null ? skyFramebuffer.getColorAttachment() : 0); }
+
     public static final RenderLayer skyRenderLayer = RenderLayer.of(MOD_ID + "_sky", VertexFormats.POSITION, VertexFormat.DrawMode.QUADS, 256, false, false,
             RenderLayer.MultiPhaseParameters.builder()
                     .program(new RenderPhase.ShaderProgram(EndlessHorizonsClient::getSkyShader))
@@ -52,9 +40,7 @@ public class EndlessHorizonsClient implements ClientModInitializer {
         BlockEntityRendererFactories.register(EndlessEndEntity, ctx -> new EndlessEndEntityRenderer());
         BlockEntityRendererFactories.register(EndlessSkyEntity, ctx -> new EndlessSkyEntityRenderer());
 
-
-
-        PostWorldRenderCallbackV2.EVENT.register(RenderSky::render);
+        WorldRenderEvents.LAST.register(RenderSky::render);
         ResolutionChangeCallback.EVENT.register(RenderSky::resetFramebuffer);
 
         CoreShaderRegistrationCallback.EVENT.register(context -> {
